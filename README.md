256a147ae4495c8c9845c35cc5e8d89786c84912
Joe Farish <joefarish@gmail.com>
2018-12-17 21:38:26 +0000

    Initalizing repository

---
+++ README.md
@@ -0,0 +1,2 @@
+# LearnPythonTheHardWay
+Repo for checking in my excercises from Learn Python the hard way
---
+++ ex3.py
@@ -0,0 +1,24 @@
+print("I will now count my chickens:")
+
+print("Hens", 25 + 30 / 6) # 25 plus 30 divided by 6
+print("Roosters", 100 - 25 * 3 % 4) # 100 minus 25 , multiplied by 3, divided by 4
+
+print("Now I will count the eggs:")
+
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6) # Add, subtract , get mod 2 - 1 and divide by 10?
+
+print("Is it true that 3 + 2 < 5 - 7?")
+
+print(3 + 2 < 5 - 7) # 5 < 2
+
+print("What is 3 + 2?", 3 + 2) #basic maths
+print("What is 5 - 7?", 5 - 7) #more basic maths
+
+print("Oh, that's why it's False.")
+
+print("How about some more.")
+
+print("Is it greater?", 5 > -2) # is 5 greater than minus 2
+print("Is it greater or equal?", 5 >= -2) # is 5 greater than or equal to  minus 2
+print("Is it less or equal?", 5 <= -2) # Is 5 less than or equal to minus 2
+
